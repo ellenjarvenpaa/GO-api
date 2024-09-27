cacdbe818a04cb1b6f2e784e2c900e5ea740279b
ellen.jarvenpaa <ellen.jarvenpaa@gmail.com>
2024-09-27 19:35:12 +0300

    working api

--- main.go
+++ main.go
@@ -5,17 +5,27 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Todo struct {
-	ID        int    `json:"_id" bson:"_id"`
-	Completed bool   `json:"completed"`
-	Body      string `json:"body"`
+type Animal struct {
+	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	AnimalName string             `json:"animal_name" bson:"animal_name"`
+	Species    string             `json:"species" bson:"species"`
+	Birthdate  time.Time          `json:"birthdate" bson:"birthdate"`
+	Location   struct {
+		Type        string     `json:"type" bson:"type"`
+		Coordinates [2]float64 `json:"coordinates" bson:"coordinates"`
+	} `json:"location" bson:"location"`
+	Owner   string `json:"owner" bson:"owner"`
+	Version int    `json:"__v" bson:"__v"`
 }
 
 var collection *mongo.Collection
@@ -35,6 +45,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	defer client.Disconnect(context.Background())
 
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
@@ -58,5 +70,108 @@ func main() {
 	}
 
 	log.Fatal(app.Listen("0.0.0.0:" + port))
+
+}
+
+func getAnimals(c *fiber.Ctx) error {
+	var animals []Animal
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+
+	if err != nil {
+		return err
+	}
+
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) {
+		var animal Animal
+		if err := cursor.Decode(&animal); err != nil {
+			return err
+		}
+		animals = append(animals, animal)
+	}
+
+	return c.JSON(animals)
+}
+
+func postAnimals(c *fiber.Ctx) error {
+	animal := new(Animal)
+
+	if err := c.BodyParser(animal); err != nil {
+		return err
+	}
+
+	if animal.AnimalName == "" {
+		return c.Status(400).JSON(fiber.Map{"error": "Animal name cannot be empty"})
+	}
+
+	insertResult, err := collection.InsertOne(context.Background(), animal)
+	if err != nil {
+		return err
+	}
+
+	animal.ID = insertResult.InsertedID.(primitive.ObjectID)
+
+	return c.Status(201).JSON(fiber.Map{"message": "Animal added successfully"})
+}
+
+func putAnimals(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid animal ID"})
+	}
+
+	var animalUpdate Animal
+	if err := c.BodyParser(&animalUpdate); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Failed to parse request body"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	update := bson.M{
+		"$set": bson.M{
+			"animal_name": animalUpdate.AnimalName,
+			"species":     animalUpdate.Species,
+			"birthdate":   animalUpdate.Birthdate,
+			"location": bson.M{
+				"type":        animalUpdate.Location.Type,
+				"coordinates": animalUpdate.Location.Coordinates,
+			},
+			"owner": animalUpdate.Owner,
+		},
+	}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Failed to update animal"})
+	}
+
+	if result.MatchedCount == 0 {
+		return c.Status(404).JSON(fiber.Map{"error": "Animal not found"})
+	}
+
+	return c.JSON(fiber.Map{
+		"message":        "Animal updated successfully",
+		"modified_count": result.ModifiedCount,
+	})
+}
+
+func deleteAnimals(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	_, err = collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"message": "Animal deleted successfully"})
 
 }
